73ff0f019b39e7ef2a3b7c069acbd50e802706b0
nidhinandepalli <nidhi.nn307@gmail.com>
2025-10-16 19:39:01 +0530

    Add files via upload

---
+++ IF_ELSE.C
@@ -0,0 +1,14 @@
+/* Name : Nidhi Nandepalli
+   UIN :251P092
+   Class : Comp/D */
+
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number: ");
+    scanf("Enter the number", &num);
+  (num % 2 == 0)?printf("Even"): printf("Odd");
+
+    return 0;
+    }
+
